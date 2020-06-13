fe80c2e60dbc1cd34ac4b200f8cfb045e765f75c
松野浩士 <amanojac52@gmail.com>
2020-06-13 17:04:39 +0900

    editとdeleteのビューを作成

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -12,7 +12,7 @@
         <div class="card-body pt-0">
           @include('error_card_list')
           <div class="card-text">
-            <form method="post" action="{{ route('articles.update', ['article' => $articles]) }}">
+            <form method="post" action="{{ route('articles.update', ['article' => $article]) }}">
               @method('PATCH')
               @include('articles.form')
               <button type="submit" class="btn blue-gradient btn-block">更新する</button>
