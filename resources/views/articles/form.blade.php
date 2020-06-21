321f6204989c6e0962c7fe16d921eb54a03785ef
松野浩士 <amanojac52@gmail.com>
2020-06-21 22:28:46 +0900

    indexとshowを追加

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -5,5 +5,5 @@
 </div>
 <div class="form-group">
   <label></label>
-  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->title ?? old('body') }}</textarea>
+  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
